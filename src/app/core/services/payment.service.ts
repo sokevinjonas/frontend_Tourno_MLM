9797bc24466ae24bc40229771c1f1beb84d4e324
sokevinjonas <sokevin7@gmail.com>
2026-01-05 23:36:54 +0000

    Remove unused `Transaction` and `WalletStats` types from payment model import.

--- src/app/core/services/payment.service.ts
+++ src/app/core/services/payment.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { Transaction, TransactionsResponse, WalletBalance, WalletStats, WalletStatisticsResponse, TournamentWallet } from '../models/payment.model';
+import { TransactionsResponse, WalletBalance, WalletStatisticsResponse, TournamentWallet } from '../models/payment.model';
 
 @Injectable({
   providedIn: 'root'
